e4925fe88d2352b1868543420b1395dad1265c68
acruikshank <acruikshank@users.noreply.github.com>
2021-04-25 23:08:23 -0400

    added robot

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import { zfn, panelGeometry } from './panels.js'
 import { Animations } from './animations.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import * as THREE from 'three'
 import * as dat from 'dat.gui'
 
@@ -10,7 +11,7 @@ import * as dat from 'dat.gui'
  * 1. [Done] create page between 3 and 4 where leftmost panel moves to center
  * 2. [Done] Add visibility to matrix and infill for page 4, and set position and rotation to that of cutout1
  * 3. [Done] use texture section from cutout 1 for infill and matrix
- * 4. Add last 5 with robot (kr quantech ultra 120)
+ * 4. [Done] Add last page with robot (kr quantech ultra 120)
  * 5. Add animation by fraction option. (with slider)
  * 6. Have animation respond to screen aspect. Update animation on resize.
  * 7. Tune aspect ratios
@@ -116,6 +117,8 @@ matrixMatFolder.addColor({matColor: matrixMat.color.getHex()}, 'matColor')
 matrixMatFolder.add(matrixMat, 'metalness').min(0).max(1).step(.0001)
 matrixMatFolder.add(matrixMat, 'roughness').min(0).max(1).step(.0001)
 
+const matrix2Mat = matrixMat.clone()
+
 const infillMat = new THREE.MeshStandardMaterial({color: 0xe6e6e6})
 infillMat.side = THREE.DoubleSide
 infillMat.transparent = true
@@ -159,6 +162,17 @@ groundGui.addColor({groundColor: groundMaterial.color.getHex()}, 'groundColor')
     .onChange((c)=>groundMaterial.color.setHex(c))
 groundMaterial.opacity = 0
 groundMaterial.transparent = true
+
+const robotMat = new THREE.MeshStandardMaterial({color: 0x252525})
+robotMat.metalness = 0
+robotMat.roughness = .71
+robotMat.transparent = true
+robotMat.opacity = 0
+const robotMatGui = gui.addFolder('robotMat')
+robotMatGui.addColor({matColor: robotMat.color.getHex()}, 'matColor')
+    .onChange((c)=>robotMat.color.setHex(c))
+    robotMatGui.add(robotMat, 'metalness').min(0).max(1).step(.0001)
+    robotMatGui.add(robotMat, 'roughness').min(0).max(1).step(.0001)
 
 
 /**
@@ -244,13 +258,161 @@ ground.position.set(50, -1.4, 22)
 ground.renderOrder = 1
 buildingGroup.add(ground)
 
-// const cutout2Group = new THREE.Group();
-// cutout2Group.add(cutout2)
-// cutout2Group.add(infillCutout)
-// cutout2Group.add(matrixCutout)
-
-// scene.add(sphere, plane, torus)
 scene.add(cutout1, cutout2, cutout3, infillCutout, matrixCutout, buildingGroup)
+
+const robotScene = new THREE.Group()
+robotScene.visible = false
+
+const matrixCutout2 = new THREE.Mesh(    
+    panelGeometry(panelWidth/3, 1.1, .2, 128, 64, 12.55, zfn(-0.999,-1,2/3,2.1)),
+    matrix2Mat
+)
+robotScene.add(matrixCutout2)
+
+const trackGeometry = new THREE.BoxBufferGeometry(5, .1, .7)
+const track1 = new THREE.Mesh(trackGeometry, robotMat)
+track1.position.set(1, -.05, -2)
+robotScene.add(track1)
+
+scene.add(robotScene)
+
+class Rotator {
+    constructor(obj, pivotPoint, axis, parent) {
+        this.obj = obj
+        this.pivotPoint = pivotPoint;
+        axis.normalize()
+        this.axis = axis
+        this._angle = 0
+
+        this.obj.parent = parent
+        parent.children.push(this.obj)
+    }
+
+    get angle() {
+        return this._angle
+    }
+
+    set angle(angle) {
+        const delta = this._angle - angle
+        this._angle = angle
+ 
+        this.obj.position.sub(this.pivotPoint)
+        this.obj.position.applyAxisAngle(this.axis, delta)
+        this.obj.position.add(this.pivotPoint)
+        this.obj.rotateOnAxis(this.axis, delta)
+    }
+}
+ 
+
+const objLoader = new OBJLoader();
+objLoader.load('./kr_120.obj', (root) => {
+    root.scale.set(.0075,.0075,.0075)
+    root.rotation.y = -Math.PI/2
+    root.position.z = -2
+    root.position.x = -.5
+
+    const xAxis = new THREE.Vector3(1,0,0)
+    const yAxis = new THREE.Vector3(0,1,0)
+    const zAxis = new THREE.Vector3(0,0,1)
+    const rotators = [
+        [1,0, new THREE.Vector3(0,0,0), yAxis], 
+        [2,1, new THREE.Vector3(75,59,0), zAxis],
+        [3,2, new THREE.Vector3(75,194,0), zAxis], 
+        [4,3, new THREE.Vector3(232,190,1), xAxis], 
+        [5,4, new THREE.Vector3(257,190,-35), zAxis]].map(r => new Rotator(root.children[r[0]], r[2], r[3], root.children[r[1]]))
+
+    root.children[0].material = robotMat
+    const rF = gui.addFolder("robot")
+    rotators.forEach((r,i)=>{ r.obj.material = robotMat })
+    rotators[0].angle = -0.31
+    rF.add(rotators[0], "angle").min(-1).max(1).step(.001)
+    rotators[1].angle = 0.316
+    rF.add(rotators[1], "angle").min(0).max(1).step(.001)
+    rotators[2].angle = 0.041
+    rF.add(rotators[2], "angle").min(-1).max(1).step(.0001)
+    rotators[3].angle = 0
+    rotators[4].angle = 1.143
+    rF.add(rotators[4], "angle").min(.5).max(1.8).step(.0001)
+
+    rF.add({leftCam: () => {camera.position.set(-6.863, 0.637, 4.156); camera.rotation.set(-0.709, -1.224, -0.679)}}, "leftCam")
+    rF.add({frontCam: () => {camera.position.set(-4.960, 0.573, 5.337); camera.rotation.set(-0.045, 0.025, 0.001)}}, "frontCam")
+    rF.add({backCam: () => {camera.position.set(-4.981, 0.532, 3.354); camera.rotation.set(-3.040, -0.037, -3.138)}}, "backCam")
+      
+    rF.add({dump: () => console.log(rotators.map(r=>r.angle))}, "dump");
+
+    // top right to left
+    const topPositions = [
+        [-0.44, 0.253, -0.021, 0, 1.40],
+        [-0.41, 0.219, 0.024,  0, 1.40],
+        [-0.37, 0.2,   0.042,  0, 1.40],
+        [-0.335,0.18,  0.065,  0, 1.4],
+        [-0.3,  0.165, 0.083,  0, 1.4148],
+        [-0.255,0.14,  0.108,  0, 1.40],
+        [-0.218,0.117, 0.1367, 0, 1.4],
+        [-0.179,0.061, 0.195,  0, 1.3268],
+        [-0.14, 0.061, 0.195,  0, 1.34],
+        [-0.1,  0.061, 0.195,  0, 1.34],
+        [-0.06, 0.061, 0.195,  0, 1.3708]]
+
+    // bottom right to left
+    const bottomPositions = [
+        [-0.443, 0.275, 0.005, 0, 1.3561],
+        [-0.409, 0.264, 0.023, 0, 1.3854],
+        [-0.373, 0.275, 0.011, 0, 1.45],
+        [-0.333, 0.25,  0.04,  0, 1.46],
+        [-0.295, 0.208, 0.0916,0, 1.40],
+        [-0.26,  0.129, 0.175, 0, 1.2975],
+        [-0.22,  0.129, 0.175, 0, 1.341],
+        [-0.18,  0.129, 0.175, 0, 1.3561],
+        [-0.138, 0.12,  0.185, 0, 1.3708],
+        [-0.095, 0.1,   0.207, 0, 1.3415],
+        [-0.05,  0.08,  0.2269,0, 1.3122]]
+    
+    const lerp = (a,b,x) => a + x*(b-a)
+
+    let position = 0
+    setInterval(() => {
+        let fraction = position % 1
+        let index = Math.floor(position) % 30
+        let startRot, endRot
+
+        if (index < 20) {
+            if (index&0x1) {
+                startRot = bottomPositions[(index+1)>>0x1]
+                endRot = topPositions[(index+1)>>0x1]
+            } else {
+                startRot = topPositions[index>>0x1]
+                endRot = bottomPositions[(index>>0x1)+1]
+            }
+        } else {
+            startRot = topPositions[30 - index]
+            endRot = topPositions[29 - index]
+        }
+        position += .02
+        rotators.forEach((r,i) => r.angle = lerp(startRot[i], endRot[i], fraction))
+    }, 20)
+
+    topPositions[0].forEach((a,i)=>rotators[i].angle = a)
+    rF.add({"topRot":0}, "topRot").min(0).max(9).step(1).onChange(v=>topPositions[v].forEach((a,i)=>rotators[i].angle = a))
+    rF.add({"botRot":0}, "botRot").min(0).max(9).step(1).onChange(v=>bottomPositions[v].forEach((a,i)=>rotators[i].angle = a))
+
+    robotScene.add(root);
+    window.robot = root
+});
+
+for (let index = 0; index<30; index++) {
+if (index < 20) {
+    if (index&0x1) {
+        console.log('start: bot', (index+1)>>0x1, ' end: top', (index+1)>>0x1)
+    } else {
+        console.log('start: top', index>>0x1, ' end: bot', (index>>0x1)+1)
+    }
+} else {
+    console.log('start: top', 30 - index, ' end: top', 29 - index)
+}
+}
+
+
 
 /**
  * Lights
@@ -333,7 +495,6 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.setClearColor(0x1f2126)
-
 
 /**
  * Animate
@@ -463,7 +624,7 @@ const pages = [{
         cutout2.visible = true
         cutout3.visible = true
         matrixCutout.visible = true
-        infillCutout.visible = true
+        infillCutout.visible = true        
     },
     params: [
         {o: camera.position,        p: {x: -0.73, y: 1.12, z: 8.36}},
@@ -491,6 +652,8 @@ const pages = [{
         console.log("PAGE 5a")
         cutout2.visible = false
         cutout3.visible = false
+        robotScene.visible = false
+        material.transparent = false        
     },
     params: [
         {o: camera.position,        p: {x: -0.73, y: 1.12, z: 8.56}},
@@ -506,18 +669,51 @@ const pages = [{
 
         {o: matrixMat,              p: {opacity:1}},
         {o: infillMat,              p: {opacity:1}},
+        {o: material,               p: {opacity:1}}
     ]
 }, {
     // page 5b (three sections of panel)
-    duration: 1,
+    duration: 3,
     complete: () => {
+        robotScene.visible = true
+        robotScene.position.x=matrixCutout.position.x - 2
+        robotScene.position.z=matrixCutout.position.z
+        material.transparent = true
     },
     params: [
+        {o: camera.position,        p: {y: 1.12, z: 8.56}, ease: power4in},
+        {o: camera.position,        p:{x: -0.73}, ease: power2out},
+        {o: camera.rotation,        p: {x: -0.26, y: 0.164, z: 0.038}},
+
         {o: cutout1.position,       p: {x:-0,      y:hoverHeight - .5, z:hoverDepth+1.75}},
         {o: infillCutout.position,  p: {x:-1.5,    y:hoverHeight - .5, z:hoverDepth+2}},
         {o: matrixCutout.position,  p: {x:-3,    y:hoverHeight - .5, z:hoverDepth+2.25}},
 
         {o: cutout1.rotation,       p: {y:rotation.y - .3}},
+        {o: matrixCutout.rotation,  p: {x: rotation.x, y:rotation.y}},
+
+        {o: matrix2Mat,             p: {opacity:0}, ease: power4out},
+        {o: robotMat,               p: {opacity:0}, ease: power4out},
+        {o: infillMat,              p: {opacity:1}, ease: power4out},
+        {o: material,               p: {opacity:1}, ease: power4out}
+    ]
+}, {
+    // page 6 (robot constructing panel)
+    duration: 2,
+    complete: () => {
+        console.log("PAGE 6")
+    },
+    params: [
+        {o: camera.position,        p: {x: -6.485, y: 1.934, z: 8.660}},
+        {o: camera.rotation,        p: {x: -0.359, y: -0.609, z: -0.211}},
+
+        {o: matrixCutout.position,  p: {x:-3,    y:hoverHeight - .5, z:hoverDepth+2.25}},
+        {o: matrixCutout.rotation,  p: {x: 0, y:0}},
+
+        {o: infillMat,              p: {opacity:0}},
+        {o: material,               p: {opacity:0}},
+        {o: matrix2Mat,             p: {opacity:1}},
+        {o: robotMat,               p: {opacity:1}},
     ]
 }]
 
@@ -525,10 +721,9 @@ const pages = [{
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 controls.addEventListener('change', ()=>{
-    // page2Camera.set(camera.position.x, camera.position.y, camera.position.z)
     page2CameraGui.updateDisplay()
-    console.log("camera position", camera.position)
-    console.log("camera rotation", camera.rotation)
+    console.log(`camera pos, x: ${camera.position.x.toFixed(3)}, y: ${camera.position.y.toFixed(3)}, z: ${camera.position.z.toFixed(3)}  
+    rot: x: ${camera.rotation.x.toFixed(3)}, y: ${camera.rotation.y.toFixed(3)}, z: ${camera.rotation.z.toFixed(3)}`)
 })
 
 const clock = new THREE.Clock()
@@ -544,6 +739,7 @@ gui.add({page3: ()=>animations.transitionToPage(3, clock.getElapsedTime())}, "pa
 gui.add({page4: ()=>animations.transitionToPage(4, clock.getElapsedTime())}, "page4")
 gui.add({page5a: ()=>animations.transitionToPage(5, clock.getElapsedTime())}, "page5a")
 gui.add({page5b: ()=>animations.transitionToPage(6, clock.getElapsedTime())}, "page5b")
+gui.add({page6: ()=>animations.transitionToPage(7, clock.getElapsedTime())}, "page6")
 
 const tick = () =>
 {    
@@ -562,5 +758,5 @@ const tick = () =>
 
 tick()
 
-animations.transitionToPage(6, clock.getElapsedTime())
+animations.transitionToPage(1, clock.getElapsedTime())
 
